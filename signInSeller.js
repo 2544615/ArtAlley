931d466b875503c6dd54bb9f1ee1d76564c91380
Mmakwena Moichela <2663211@students.wits.ac.za>
2025-04-13 13:37:10 +0200

    Sign in for buyer and seller are done

---
+++ signInSeller.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUfE0XLFPlpw_SAJIFoQlJhylk-r2VY4Y",
+  authDomain: "artalley-b9c96.firebaseapp.com",
+  projectId: "artalley-b9c96",
+  storageBucket: "artalley-b9c96.firebasestorage.app",
+  messagingSenderId: "1056868925602",
+  appId: "1:1056868925602:web:4fa9734632b255594917fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submit = document.querySelector('.login-btn');
+
+
+submit.addEventListener('click', function(event){
+    event.preventDefault();
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    signInWithEmailAndPassword(auth, emailValue, passwordValue)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert('checking user info...');
+    console.log('user signed in');
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+  });
+})
