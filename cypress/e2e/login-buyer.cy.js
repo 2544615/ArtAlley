811b3045789a12caeae30fea8dda08ba040ed506
Oneilwe123 <2117099@students.wits.ac.za>
2025-04-13 23:53:24 +0200

    specs for log in

---
+++ cypress/e2e/login-buyer.cy.js
@@ -0,0 +1,51 @@
+describe('Login Page', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/SignIn Folder/login-buyer.html'); 
+    });
+  
+    it('Loads the login form correctly', () => {
+      cy.contains('Login as a buyer').should('be.visible');
+      cy.contains('Welcome back').should('be.visible');
+    });
+  
+    it('Has email and password fields', () => {
+      cy.get('input#email')
+        .should('exist')
+        .and('have.attr', 'type', 'email')
+        .and('have.attr', 'placeholder', 'Enter your email here');
+  
+      cy.get('input#password')
+        .should('exist')
+        .and('have.attr', 'type', 'password')
+        .and('have.attr', 'placeholder', '**********');
+    });
+  
+    it('Login button is present and functional', () => {
+      cy.get('button.login-btn')
+        .should('exist')
+        .and('contain', 'Login');
+    });
+  
+    it('Forgot Password link is correct', () => {
+      cy.get('nav.options a')
+        .should('exist')
+        .and('have.attr', 'href', 'ForgotPaswword.html'); 
+    });
+  
+    it('Google sign-in section is visible', () => {
+      cy.get('#google-btn')
+        .should('exist')
+        .and('contain', 'Sign in with Google');
+  
+      cy.get('#google-btn img')
+        .should('have.attr', 'src')
+        .and('include', 'google-logo.png');
+    });
+  
+    it('Sign up link goes to buyer signup', () => {
+      cy.get('footer .signup a')
+        .should('exist')
+        .and('have.attr', 'href', '../SignUp Folder/buyer-signup.html');
+    });
+  });
+  
