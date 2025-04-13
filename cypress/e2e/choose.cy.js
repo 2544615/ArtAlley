10affd8db7438455474a9e8655ec282a53a000a7
Oneilwe123 <2117099@students.wits.ac.za>
2025-04-13 23:20:33 +0200

    Update choose.cy.js

--- cypress/e2e/choose.cy.js
+++ cypress/e2e/choose.cy.js
@@ -17,7 +17,6 @@ describe('Choose Role Page - ArtAlley', () => {
     });
   
     it('Redirects to seller login when Seller is selected', () => {
-      // Fix the typo in your HTML value (".../SignIn") before running this
       cy.get('#roleSelect').select('../SignIn Folder/login-seller.html');
       cy.get('form#roleForm').submit();
       cy.url().should('include', 'login-seller.html');
