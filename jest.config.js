cd8635f49f1a7d4c6301703fe36de905dac68afc
Mmakwena Moichela <2663211@students.wits.ac.za>
2025-04-20 12:57:23 +0200

    testing exhausted

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: 'node', // or 'jsdom' for browser-like env
+  testEnvironment: 'jsdom', // or 'jsdom' for browser-like env
   testMatch: ['**/*.test.js', '**/*.spec.js'], // test file patterns
   collectCoverage: true, // enable coverage
   coverageDirectory: 'coverage', // where coverage reports go
